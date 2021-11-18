9869155f29ec8919bc3384cf8af7729ad2a6f98f
harry <harry.gambot@gmail.com>
2021-11-18 07:51:45 +0700

    Completed redux for add and removing item on cart

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,41 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 7,
+    title: "Cookie Jam",
+    description: "Best cookie jam in town!",
+  },
+  {
+    id: "p2",
+    price: 4,
+    title: "Strawberry Jam",
+    description: "Best strawberry jam in town!",
+  },
+  {
+    id: "p3",
+    price: 8,
+    title: "Grape Jam",
+    description: "Best grape jam in town!",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            id={product.id}
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
