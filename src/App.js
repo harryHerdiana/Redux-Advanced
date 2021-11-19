144b29bdc40eeb9b72d5c88984ea27f1376474fa
Harry <harry.gamebot@gmail.com>
2021-11-19 21:48:17 +0700

    Completed redux async functions and fetching to firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
-import { useSelector } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useSelector, useDispatch } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useEffect, Fragment } from "react";
+import { fetchCartData } from "./store/cart-actions";
+import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-actions";
+
+let isInitial = true;
 
 function App() {
-  const showCart = useSelector((state)=>state.ui.isVisible)
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.isVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+  
+  useEffect(()=>{
+    dispatch(fetchCartData())
+  },[dispatch])
+  
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if (cart.changed){
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -2,13 +2,20 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
   name: "ui",
-  initialState: { isVisible: false },
+  initialState: { isVisible: false, notification: null },
   reducers: {
     toggle(state) {
       state.isVisible = !state.isVisible;
     },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
   },
 });
-export const uiActions = uiSlice.actions
+export const uiActions = uiSlice.actions;
 
-export default uiSlice
+export default uiSlice;
